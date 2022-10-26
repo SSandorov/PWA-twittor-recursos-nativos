ce0f32c8b6a2df559699ce2f423ba05de64d7913
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-10-26 19:14:27 +0200

    innit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 const publicPath = path.resolve(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 // app.use(bodyParser.json()); // support json encoded bodies
